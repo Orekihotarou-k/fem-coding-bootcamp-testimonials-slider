7eaea9a08fc8d00d93b2298c1c4366576e9cee46
Levis Kim <122298089+Orekihotarou-k@users.noreply.github.com>
2024-02-19 12:44:17 +0100

    update index.html

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const testimonials = document.querySelectorAll('.testimonial');
-let currentTestimonial = 0;
+// const testimonials = document.querySelectorAll('.testimonial');
+// let currentTestimonial = 0;
 
-function showNextTestimonial() {
-    if (currentTestimonial < testimonials.length - 1) {
-        testimonials[currentTestimonial].classList.add('hidden');
-        currentTestimonial++;
-        testimonials[currentTestimonial].classList.remove('hidden');
-    }
-}
+// function showNextTestimonial() {
+//     if (currentTestimonial < testimonials.length - 1) {
+//         testimonials[currentTestimonial].classList.add('hidden');
+//         currentTestimonial++;
+//         testimonials[currentTestimonial].classList.remove('hidden');
+//     }
+// }
 
-function showPreviousTestimonial() {
-    if (currentTestimonial > 0) {
-        testimonials[currentTestimonial].classList.add('hidden');
-        currentTestimonial--;
-        testimonials[currentTestimonial].classList.remove('hidden');
-    }
-}
+// function showPreviousTestimonial() {
+//     if (currentTestimonial > 0) {
+//         testimonials[currentTestimonial].classList.add('hidden');
+//         currentTestimonial--;
+//         testimonials[currentTestimonial].classList.remove('hidden');
+//     }
+// }
