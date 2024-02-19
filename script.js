f80f31bf99d725ffccfd44d22ca1cceea76e8858
Levis Kim <122298089+Orekihotarou-k@users.noreply.github.com>
2024-02-19 17:58:51 +0100

    html/css/js update

--- script.js
+++ script.js
@@ -16,3 +16,22 @@
 //         testimonials[currentTestimonial].classList.remove('hidden');
 //     }
 // }
+
+const testimonial = document.querySelectorAll(".slide-col")
+let currentTestimonial = 0
+
+function showNextTestimonial() {
+    if (currentTestimonial > 0) {
+        testimonial[currentTestimonial].classList.add("hidden")
+        currentTestimonial--
+    }
+    testimonial[currentTestimonial].classList.remove("hidden")
+}
+
+function showPreviousTestimonial() {
+    if (currentTestimonial < testimonial.length - 1) {
+        testimonial[currentTestimonial].classList.add("hidden")
+        currentTestimonial++
+    }
+    testimonial[currentTestimonial].classList.remove("hidden")
+}
